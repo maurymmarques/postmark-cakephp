f877334b4de60a31ff3a4b8113c694d37e541a05
Maury M. Marques <maurymmarques@gmail.com>
2011-11-15 22:47:20 -0200

    Postmark Transport for CakePHP

---
+++ app/Lib/Network/Email/PostmarkTransport.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Postmark Transport
+ *
+ * Copyright 2011, Maury M. Marques
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author	Maury M. Marques
+ * @copyright	Copyright 2011, Maury M. Marques
+ * @version	0.1
+ * @license	http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+App::uses('AbstractTransport', 'Network/Email');
+App::uses('HttpSocket', 'Network/Http');
+
+/**
+ * PostmarkTransport
+ *
+ * This class is used for sending email messages
+ * using the Postmark API http://postmarkapp.com/
+ *
+ */
+class PostmarkTransport extends AbstractTransport {
+
+	/**
+	 * CakeEmail
+	 *
+	 * @var CakeEmail
+	 */
+	protected $_cakeEmail;
+
+	/**
+	 * Variable that holds Postmark connection
+	 *
+	 * @var HttpSocket
+	 */
+	private $__postmarkConnection;
+
+	/**
+	 * CakeEmail headers
+	 *
+	 * @var array
+	 */
+	protected $_headers;
+
+	/**
+	 * Configuration to transport
+	 *
+	 * @var mixed
+	 */
+	protected $_config = array();
+
+	/**
+	 * Sends out email via Postmark
+	 *
+	 * @return array Return the Postmark
+	 */
+	public function send(CakeEmail $email) {
+		// CakeEmail
+		$this->_cakeEmail = $email;
+
+		$this->_config = $this->_cakeEmail->config();
+		$this->_headers = $this->_cakeEmail->getHeaders(array('from', 'to', 'cc', 'bcc', 'replyTo', 'subject'));
+
+		// Setup connection
+		$this->__postmarkConnection = & new HttpSocket();
+
+		// Build message
+		$message = $this->__buildMessage();
+
+		// Build request
+		$request = $this->__buildRequest();
+
+		// Send message
+		$returnPostmark = $this->__postmarkConnection->post($this->_config['uri'], json_encode($message), $request);
+
+		return json_decode($returnPostmark, true);
+	}
+
+	/**
+	 * Build message
+	 *
+	 * @return array
+	 */
+	private function __buildMessage() {
+		// Message
+		$message = array();
+
+		// From
+		$message['From'] = $this->_headers['From'];
+
+		// To
+		$message['To'] = $this->_headers['To'];
+
+		// Cc
+		$message['Cc'] = $this->_headers['Cc'];
+
+		// Bcc
+		$message['Bcc'] = $this->_headers['Bcc'];
+
+		// ReplyTo
+		$message['ReplyTo'] = $this->_headers['Reply-To'];
+
+		// Subject
+		$message['Subject'] = $this->_headers['Subject'];
+
+		// HtmlBody
+		if ($this->_cakeEmail->emailFormat() === 'html' || $this->_cakeEmail->emailFormat() === 'both') {
+			$message['HtmlBody'] = $this->_cakeEmail->message($this->_cakeEmail->emailFormat());
+		}
+
+		// TextBody
+		$message['TextBody'] = implode('', $this->_cakeEmail->message());
+
+		// Attachments
+		$message['Attachments'] = $this->__buildAttachments();
+
+		return $message;
+	}
+
+	/**
+	 * Build attachments
+	 *
+	 * @return array
+	 */
+	private function __buildAttachments() {
+		// Attachments
+		$attachments = array();
+
+		$i = 0;
+		foreach ($this->_cakeEmail->attachments() as $fileName => $fileInfo) {
+
+			$handle = fopen($fileInfo['file'], 'rb');
+			$data = fread($handle, filesize($fileInfo['file']));
+			$data = chunk_split(base64_encode($data)) ;
+			fclose($handle);
+
+			$attachments[$i]['Name'] = $fileName;
+			$attachments[$i]['Content'] = $data;
+			$attachments[$i]['ContentType'] = $fileInfo['mimetype'];
+
+			$i++;
+		}
+
+		return $attachments;
+	}
+
+	/**
+	 * Build request
+	 *
+	 * @return array
+	 */
+	private function __buildRequest () {
+		$request = array(
+			'header' => array(
+				'Accept' => 'application/json',
+				'Content-Type' => 'application/json',
+				'X-Postmark-Server-Token' => $this->_config['key']
+			)
+		);
+
+		return $request;
+	}
+
+}
---
+++ app/Test/Case/Network/Email/PostmarkTransportTest.php
@@ -0,0 +1,55 @@
+<?php
+App::uses('CakeEmail', 'Network/Email');
+
+/**
+ * Test case
+ *
+ */
+class PostmarkTransportTest extends CakeTestCase {
+
+/**
+ * CakeEmail
+ *
+ * @var CakeEmail
+ */
+	private $email;
+
+/**
+ * Setup
+ *
+ * @return void
+ */
+	public function setUp() {
+		$this->email = new CakeEmail();
+	}
+
+/**
+ * testPostmarkSend method
+ *
+ * @return void
+ */
+	public function testPostmarkSend() {
+		$this->email->transport('Postmark');
+		$this->email->config('postmark');
+		$this->email->template('default', 'default');
+		$this->email->emailFormat('html');
+		$this->email->from(array('yourpostmark@mail.com' => 'Your Name'));
+		$this->email->to(array('recipient@domain.com' => 'Recipient'));
+		$this->email->cc(array('recipient@domain.com' => 'Recipient'));
+		$this->email->bcc(array('recipient@domain.com' => 'Recipient'));
+		$this->email->subject('Test Postmark');
+		$this->email->attachments(array(
+		    'cake.icon.png' => array(
+		        'file' => WWW_ROOT . 'img' . DS . 'cake.icon.png'
+			)
+		));
+
+		$sendReturn =  $this->email->send();
+
+		$headers = $this->email->getHeaders(array('to'));
+		$this->assertEqual($sendReturn['To'], $headers['To']);
+		$this->assertEqual($sendReturn['ErrorCode'], 0);
+		$this->assertEqual($sendReturn['Message'], 'OK');
+	}
+
+}
